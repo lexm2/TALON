588885638395ba06c14b482c275a0b22418c75d9
lexm2 <lex.k.moulton@gmail.com>
2024-06-19 11:08:20 -0400

    Quality of life changes

--- game/game.py
+++ game/game.py
@@ -2,113 +2,69 @@ import random
 
 
 class Game:
-    def __init__(self, min_value=0, max_value=6, max_attempts=6):
+    def __init__(self, min_value=0, max_value=6, max_attempts=6, num_episodes=10000):
         self.min_value = min_value
         self.max_value = max_value
         self.max_attempts = max_attempts
+        self.num_episodes = num_episodes
         self.target_number = None
         self.attempts_left = None
-        self.guess1 = None
-        self.guess1eval = None
-        self.guess2 = None
-        self.guess2eval = None
-        self.guess3 = None
-        self.guess3eval = None
-        self.guess4 = None
-        self.guess4eval = None
-        self.guess5 = None
-        self.guess5eval = None
-        self.guess6 = None
-        self.guess6eval = None
+        self.guesses = []
+        self.evals = []
         self.last_guess = None
+        self.higher_lower = None
 
     def reset(self):
         self.target_number = random.randint(self.min_value, self.max_value)
         self.attempts_left = self.max_attempts
-        self.guess1 = -1
-        self.guess1eval = 0
-        self.guess2 = -1
-        self.guess2eval = 0
-        self.guess3 = -1
-        self.guess3eval = 0
-        self.guess4 = -1
-        self.guess4eval = 0
-        self.guess5 = -1
-        self.guess5eval = 0
-        self.guess6 = -1
-        self.guess6eval = 0
+        self.guesses = [-1] * self.max_attempts
+        self.evals = [0] * self.max_attempts
         self.last_guess = -1
         
         state = [
             self.min_value,
             self.max_value,
             self.attempts_left,
-            self.guess1,
-            self.guess1eval,
-            self.guess2,
-            self.guess2eval,
-            self.guess3,
-            self.guess3eval,
-            self.guess4,
-            self.guess4eval,
-            self.guess5,
-            self.guess5eval,
-            self.guess6,
-            self.guess6eval,
+            *self.guesses,
+            *self.evals,
         ]
         return state
 
     def step(self, guess):
-        higher_lower = 0
-        
         self.attempts_left -= 1
+        index = self.max_attempts - self.attempts_left - 1
+        self.guesses[index] = guess
         
+
+        
+        if self.num_episodes < 11:
+            print(str(guess) + " " + str(self.target_number))
         if guess == self.target_number:
-            # reward = 10 / (self.max_attempts / (self.attempts_left + 1))  # Smaller attempts left yields higher reward
             reward = 10
             done = True
             feedback = [0]
         else:
-            distance = abs(guess - self.target_number)
-            # reward = 3 / (distance + 1)  # Smaller distance yields higher reward
             reward = 0
             if guess == self.last_guess:
                 reward = -10
                 done = True
             if guess < self.target_number:
                 done = False
-                higher_lower = -1
+                self.higher_lower = -1
             else:
                 done = False
-                higher_lower = 1
-            feedback = [higher_lower]     
-        self.last_guess = guess
-            
-        if self.attempts_left == 5:
-            self.guess1 = guess
-            self.guess1eval = higher_lower
-        elif self.attempts_left == 4:
-            self.guess2 = guess
-            self.guess2eval = higher_lower
-        elif self.attempts_left == 3:
-            self.guess3 = guess
-            self.guess3eval = higher_lower
-        elif self.attempts_left == 2:
-            self.guess4 = guess
-            self.guess4eval = higher_lower
-        elif self.attempts_left == 1:
-            self.guess5 = guess
-            self.guess5eval = higher_lower
-        elif self.attempts_left == 0:
-            self.guess6 = guess
-            self.guess6eval = higher_lower
-            done = True
+                self.higher_lower = 1
+            feedback = [self.higher_lower]
 
+        if self.attempts_left == 0:
+            done = True
+        
+        self.evals[index] = self.higher_lower if self.higher_lower is not None else 0
+        self.last_guess = guess
         return reward, done, feedback
 
     def render(self):
         pass
-        # print(f"Attempts left: {self.attempts_left}")
 
     def is_done(self):
         return self.attempts_left == 0
--- main.py
+++ main.py
@@ -4,38 +4,35 @@ from game import Game
 from nn import DQN
 from rl import Agent
 from tqdm import tqdm
+from collections import deque
 import torch
 import argparse
+import numpy as np
+import multiprocessing
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def main():
 
-    parser = argparse.ArgumentParser(description="Number Guessing Game with Reinforcement Learning")
-    parser.add_argument(
-        "-e", "--episodes", type=int, default=100000, help="Number of training episodes (default: 100000)"
-    )
-    parser.add_argument("-s", "--save", type=int, default=10000, help="How often to make saves")
-    parser.add_argument(
-        "--manual",
-        "-m",
-        type=int,
-        nargs=3,
-        metavar=("ATTEMPTS", "LASTGUESS", "HIGHERLOWER"),
-        help="Manualy run the neural network with the given parameters",
-    )
-    parser.add_argument("--write", "-w", action="store_true", help="Write output to a file")
-    parser.add_argument("--overwrite", "-o", action="store_true", help="Overwrite the save file")
-    args = parser.parse_args()
-
-    input_size = 15  # min_value, max_value, attempts_left, guesses, higher_lower
-    hidden_sizes = [16, 64, 16]  # BS sizes
+    game = Game(min_value=0, max_value=6)
+    args = parse_args()
+    input_size = 3 + 2 * game.max_attempts  # min_value, max_value, attempts_left, guesses, higher_lower
+    hidden_sizes = [16, 64, 128]  # BS sizes
     output_size = 7
     learning_rate = 0.002
     discount_factor = 0.99
     epsilon = 0.8
+    model = DQN(input_size, hidden_sizes, output_size, device)
+    agent = Agent(model, learning_rate, discount_factor, epsilon, device)
     num_episodes = args.episodes
+    window_size = num_episodes / 100
+    rewards = deque(maxlen=window_size)
+    attempts = deque(maxlen=window_size)
+    reward_avgs = []
+    attempts_avgs = []
+    num_processes = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=num_processes)
 
     if args.write:
         output_file = open("output.txt", "w")
@@ -43,9 +40,6 @@ def main():
         output_file = None
 
     print("Using " + device.type)
-    game = Game(min_value=0, max_value=6)
-    model = DQN(input_size, hidden_sizes, output_size, device)
-    agent = Agent(model, learning_rate, discount_factor, epsilon, device)
 
     checkpoint_path = "chekcpoint.pt"
     if os.path.exists(checkpoint_path) and not args.overwrite:
@@ -63,9 +57,6 @@ def main():
             else:
                 print("Failed to load model parameters from checkpoint. Will overwrite them.")
 
-    rewards = []
-    reward_avgs = []
-
     progress_bar = tqdm(range(num_episodes), desc="Training Episodes", total=num_episodes)
 
     for episode in range(num_episodes):
@@ -76,38 +67,27 @@ def main():
         while not done:
             action = agent.select_action(state)
             reward, done, feedback = game.step(action)
-            next_state = [
-                game.min_value,
-                game.max_value,
-                game.attempts_left,
-                game.guess1,
-                game.guess1eval,
-                game.guess2,
-                game.guess2eval,
-                game.guess3,
-                game.guess3eval,
-                game.guess4,
-                game.guess4eval,
-                game.guess5,
-                game.guess5eval,
-                game.guess6,
-                game.guess6eval,
-            ]
+            next_state = np.concatenate(
+                ([game.min_value, game.max_value, game.attempts_left], game.guesses, game.evals)
+            )
             agent.train(state, action, reward, next_state, done, feedback)
             state = next_state
             episode_reward += reward
 
         rewards.append(episode_reward)
+        attempts.append(game.max_attempts - game.attempts_left)
         progress_bar.update()
-        if (episode + 1) % (num_episodes / 100) == 0:
-            reward_avgs.append(round(number=sum(rewards) / len(rewards), ndigits=7))
-        if (episode + 1) % args.save == 0:
+        if (episode + 1) % window_size == 0:
+            reward_avgs.append(round(calculate_rolling_average(rewards, window_size), ndigits=7))
+            attempts_avgs.append(round(calculate_rolling_average(attempts, window_size), ndigits=7))
+        if (episode + 1) % args.save == 0 and not num_episodes <= args.save:
             save_model_and_test_inputs(model, checkpoint_path, output_file=output_file)
         # print(f"Episode {episode + 1}: Attempts left = {game.attempts_left}")
 
     save_model_and_test_inputs(model, checkpoint_path=checkpoint_path, output_file=output_file)
 
-    plot_reward_graph(reward_avgs)
+    plot_reward_graph(reward_avgs, attempts_avgs)
+    output_file.close()
 
 
 def save_model_and_test_inputs(model, checkpoint_path, output_file=None):
@@ -117,13 +97,11 @@ def save_model_and_test_inputs(model, checkpoint_path, output_file=None):
     if output_file is not None:
         output_file.write("Model Architecture:\n")
         output_file.write(str(model) + "\n")
-        output_file.write("\nModel State Dict:\n")
-        output_file.write(str(model.state_dict()) + "\n")
 
         test_inputs = [
-            [0, 6, 6, -1, 0, -1, 0, -1, 0, -1, 0, -1, 0, -1, 0],  # Test input 1
-            [0, 6, 4, 3, -1, -1, 0, -1, 0, -1, 0, -1, 0, -1, 0],  # Test input 2
-            [0, 6, 2, 5, 1, 3, -1, -1, 0, -1, 0, -1, 0, -1, 0],  # Test input 3
+            [0, 6, 6] + [-1] * 6 + [0] * 6,  # Test input 1
+            [0, 6, 4, 3] + [-1] * 5 + [-1] + [0] * 5,  # Test input 2
+            [0, 6, 2, 5, 3] + [-1] * 4 + [-1, 1] + [0] * 4,  # Test input 3
         ]
 
         output_file.write("\nInput Tests:\n")
@@ -132,14 +110,56 @@ def save_model_and_test_inputs(model, checkpoint_path, output_file=None):
             output = model(test_input_tensor)
             output_file.write(f"Test Input {i+1}: {test_input}\n")
             output_file.write(f"Output: {output.tolist()}\n")
+            output_file.write(f"Expected: {output.tolist()}\n")
 
 
-def plot_reward_graph(reward_avgs):
-    plt.plot(reward_avgs)
-    plt.xlabel("Episode")
-    plt.ylabel("Reward average")
-    plt.title("Reward average over Episodes")
-    plt.savefig("reward_plot.png")
+def plot_reward_graph(reward_avgs, attempts_avgs):
+    fig, ax1 = plt.subplots()
+
+    # Plot reward average on the primary y-axis
+    ax1.plot(reward_avgs, color="blue")
+    ax1.set_xlabel("Episode")
+    ax1.set_ylabel("Reward Average", color="blue")
+    ax1.tick_params("y", colors="blue")
+
+    # Create a secondary y-axis
+    ax2 = ax1.twinx()
+
+    # Plot attempt average on the secondary y-axis
+    ax2.plot(attempts_avgs, color="red")
+    ax2.set_ylabel("Attempt Average", color="red")
+    ax2.tick_params("y", colors="red")
+
+    plt.title("Reward and Attempt Averages over Episodes")
+    plt.tight_layout()
+    plt.savefig("reward_attempt_plot.png")
+    plt.close()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Number Guessing Game with Reinforcement Learning")
+    parser.add_argument(
+        "-e", "--episodes", type=int, default=100000, help="Number of training episodes (default: 100000)"
+    )
+    parser.add_argument("-s", "--save", type=int, default=10000, help="How often to make saves")
+    parser.add_argument(
+        "--manual",
+        "-m",
+        type=int,
+        nargs=3,
+        metavar=("ATTEMPTS", "LASTGUESS", "HIGHERLOWER"),
+        help="Manualy run the neural network with the given parameters",
+    )
+    parser.add_argument("--write", "-w", action="store_true", help="Write output to a file")
+    parser.add_argument("--overwrite", "-o", action="store_true", help="Overwrite the save file")
+    return parser.parse_args()
+
+
+def calculate_rolling_average(values, window_size):
+    if len(values) < window_size:
+        return sum(values) / len(values)
+    else:
+        return sum(values[-int(window_size) :]) / window_size
 
 
 if __name__ == "__main__":
